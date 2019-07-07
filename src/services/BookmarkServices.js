6322b486262c1271ad84144f8ea16ed49d76722e
Zixia Lu <zixialu@gmail.com>
2019-07-07 02:36:10 -0400

    Create service serving fake data

---
+++ src/services/BookmarkServices.js
@@ -0,0 +1,26 @@
+function get() {
+  // TODO: Replace this with an actual request
+  const data = [
+    {
+      id: '1a',
+      name: 'Google',
+      uri: 'https://google.ca',
+      notes: 'Give it a goog',
+      tags: ['test', 'search'],
+      createdAt: new Date(),
+    },
+    {
+      id: '1b',
+      name: 'Reddit',
+      uri: 'http://reddit.com',
+      notes: 'The front page of the internet',
+      tags: ['test'],
+      createdAt: new Date(),
+    },
+  ];
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve(data), 1000);
+  });
+}
+
+export default { get };
