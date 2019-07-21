0f54a5e7aec65f93a42aec13a20f3078335bc17c
Zixia Lu <zixialu@gmail.com>
2019-07-21 13:31:17 -0400

    Create some user route logic

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const UserService = require('../services/UserService');
 
 const router = express.Router();
 
 module.exports = (knex) => {
-  router.get('/', (req, res) => {
-    res.send('respond with a resource');
+  const User = UserService(knex);
+
+  router.param('userId', async (req, res, next, userId) => {
+    try {
+      const foundUser = await User.get(userId);
+      if (foundUser) {
+        req.user = foundUser;
+        return next();
+      }
+      return next(new Error('failed to load user'));
+    } catch (err) {
+      return next(err);
+    }
+  });
+
+  // GET /users/:userId
+  router.get('/:userId', (req, res) => {
+    res.json(req.user);
   });
 
   return router;
