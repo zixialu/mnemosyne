40486be4b19dbc1d4ffd3c84438fbc7b1e4b4f8c
Zixia Lu <zixialu@gmail.com>
2019-07-21 17:52:58 -0400

    Install react router, refactor mnemosyne from app to its own component in preparation for routing

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,55 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import BookmarkService from '../services/BookmarkServices';
-import Header from './Header';
-import Sidebar from './Sidebar';
-import BookmarkSearch from './BookmarkSearch';
-import BookmarkList from './BookmarkList';
-import DetailsPane from './DetailsPane';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Mnemosyne from './Mnemosyne';
 import '../styles/app.scss';
 
 function App() {
-  const [bookmarks, setBookmarks] = useState(null);
-  const [search, setSearch] = useState('');
-  const [filteredBookmarks, setFilteredBookmarks] = useState(null);
-
-  // Get bookmarks
-  useEffect(() => {
-    let ignore = false;
-
-    async function getBookmarks() {
-      const bookmarks = await BookmarkService.getBookmarks();
-      if (!ignore) setBookmarks(bookmarks);
-    }
-    getBookmarks();
-
-    return () => { ignore = true; };
-  }, [])
-
-  // Filter bookmarks
-  useEffect(() => {
-    function filterBookmarks() {
-      const lowerCaseSearch = search.toLowerCase()
-      setFilteredBookmarks(bookmarks && bookmarks.filter(bookmark => (
-        bookmark.name
-          .toLowerCase()
-          .includes(lowerCaseSearch)
-        || bookmark.tags
-          .map(tag => tag.toLowerCase())
-          .includes(lowerCaseSearch)
-      )));
-    }
-    filterBookmarks();
-  }, [bookmarks, search])
-
   return (
-    <div className="app">
-      <Header />
-      <Sidebar />
-      <BookmarkSearch update={setSearch} />
-      <BookmarkList bookmarks={filteredBookmarks} />
-      <DetailsPane />
-    </div>
-  );
+    <Mnemosyne />
+  )
 }
 
 export default App;
