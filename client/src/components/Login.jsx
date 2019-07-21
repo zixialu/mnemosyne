db9b0a9db3f0c37a74167fc8db8b87c34f44e20c
Zixia Lu <zixialu@gmail.com>
2019-07-21 18:47:05 -0400

    Install axios, add some login logic

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import Button from './Button';
 import '../styles/login.scss'
 
 function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [attemptingLogin, setAttemptingLogin] = useState(false);
 
   const validateForm = () => username.length && password.length;
 
@@ -16,42 +18,56 @@ function Login() {
     setPassword(value);
   }
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
+    setAttemptingLogin(true);
+
+    try {
+      // TODO: Login
+    } catch (err) {
+      alert(err.message);
+    }
   }
 
   return (
     <div className="login">
-      <form onSubmit={handleSubmit}>
-        <div className="input-group">
-          <label for="username">Username</label>
-          <input
-            type="text"
-            id="username"
-            placeholder="Username"
-            value={username}
-            onChange={handleUsernameChange}
-          />
-        </div>
+      {attemptingLogin
+        ? (
+          <div>...</div>
+        )
+        : (
+          <form onSubmit={handleSubmit}>
+            <div className="input-group">
+              <label for="username">Username</label>
+              <input
+                autofocus
+                type="text"
+                id="username"
+                placeholder="Username"
+                value={username}
+                onChange={handleUsernameChange}
+              />
+            </div>
 
-        <div className="input-group">
-          <label for="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            placeholder="Password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-        </div>
+            <div className="input-group">
+              <label for="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                placeholder="Password"
+                value={password}
+                onChange={handlePasswordChange}
+              />
+            </div>
 
-        <Button
-          type="submit"
-          disabled={!validateForm()}
-        >
-          Login
+            <Button
+              type="submit"
+              disabled={!validateForm()}
+            >
+              Login
         </Button>
-      </form>
+          </form>
+        )}
     </div>
   )
 }
