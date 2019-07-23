65d15f6125c5386c7e764a43781f83e3d7bbe7c8
Zixia Lu <zixialu@gmail.com>
2019-07-22 21:32:16 -0400

    Create auth state

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Mnemosyne from './Mnemosyne';
 import Login from './Login';
 import '../styles/app.scss';
 
 function App() {
+  const [auth, setAuth] = useState({
+    isAuthenticated: false,
+  });
+
   return (
     <Router>
       <Route path="/" exact component={Mnemosyne} />
