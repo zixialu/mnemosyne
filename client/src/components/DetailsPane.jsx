dd498bad4cd32d55c73b3cf8d7281a31b7658e02
Zixia Lu <zixialu@gmail.com>
2019-07-25 00:15:57 -0400

    Implement slide-in details transition

--- client/src/components/DetailsPane.jsx
+++ client/src/components/DetailsPane.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import '../styles/detailsPane.scss';
 
 function DetailsPane({ bookmark }) {
-  return bookmark && (
-    <div className="details-pane">
-      Details Pane
-    </div>
+
+  return (
+    <TransitionGroup component={null}>
+      {bookmark && (
+        <CSSTransition in={Boolean(bookmark)} timeout={200} classNames="slide">
+          <div className="details-pane">
+            <h3>Details Pane</h3>
+            {JSON.stringify(bookmark)}
+          </div>
+        </CSSTransition>
+      )}
+    </TransitionGroup>
   );
 }
 
