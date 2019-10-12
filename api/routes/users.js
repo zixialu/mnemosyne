661dd44494a3565a543d866d0b5ce335f7fef0c2
Zixia Lu <zixialu@gmail.com>
2019-10-12 11:23:13 -0400

    More work on session auth

--- api/routes/users.js
+++ api/routes/users.js
@@ -9,19 +9,19 @@ module.exports = (knex) => {
   const User = UserService(knex);
 
   // Handle param userId
-  router.param('userId', async (req, res, next, userId) => {
-    try {
-      const foundUser = await User.get(userId);
-      if (foundUser) {
-        req.user = foundUser;
-        return next();
-      }
-      return next(createError(404, 'User not found'));
-    } catch (err) {
-      console.error(err);
-      return next(err);
-    }
-  });
+  // router.param('userId', async (req, res, next, userId) => {
+  //   try {
+  //     const foundUser = await User.get(userId);
+  //     if (foundUser) {
+  //       req.user = foundUser;
+  //       return next();
+  //     }
+  //     return next(createError(404, 'User not found'));
+  //   } catch (err) {
+  //     console.error(err);
+  //     return next(err);
+  //   }
+  // });
 
   // POST /users
   // Create new user
@@ -30,6 +30,21 @@ module.exports = (knex) => {
       const { username, email, password } = req.body;
       const createdUser = await User.create({ username, email, password });
       return res.json(createdUser);
+    } catch (err) {
+      console.error(err);
+      return next(err);
+    }
+  });
+
+  router.post('/login', async (req, res, next) => {
+    try {
+      const { username, password } = req.body;
+      const verifiedUser = await User.verify(username, password);
+      if (verifiedUser) {
+        SessionService.addUserToSession();
+        return res.sendStatus(200);
+      }
+      return res.sendStatus(401);
     } catch (err) {
       console.error(err);
       return next(err);
--- api/services/UserService.js
+++ api/services/UserService.js
@@ -1,7 +1,10 @@
+const bcrypt = require('bcrypt');
+
+const { SALT_ROUNDS } = process.env;
+
 module.exports = knex => ({
   async create({ username, email, password }) {
-    // TODO: Hash password
-    const passwordHash = password;
+    const passwordHash = await bcrypt.hash(password, SALT_ROUNDS);
 
     return knex('users')
       .insert({
@@ -14,12 +17,36 @@ module.exports = knex => ({
   async get(id) {
     return knex('users')
       .select('id', 'username', 'email')
-      .where({ id });
+      .where({ id })
+      .first();
+  },
+
+  async findOne(username) {
+    return knex('users')
+      .select('id', 'username', 'email')
+      .where({ username })
+      .first();
+  },
+
+  async verify(username, password) {
+    const recordedHash = await knex('users')
+      .select('id', 'username', 'email', 'password_hash')
+      .where({ username })
+      .first();
+
+    const isValid = await bcrypt.compare(password, recordedHash);
+    if (isValid) {
+      return {
+        id: recordedHash.id,
+        username,
+        email: recordedHash.email,
+      };
+    }
+    return false;
   },
 
   async changePassword({ id, password }) {
-    // TODO: Hash password
-    const passwordHash = password;
+    const passwordHash = await bcrypt.hash(password, SALT_ROUNDS);
 
     return knex('users')
       .where({ id })
